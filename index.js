27f7ef18c64da99521f84b7aa05e7af9c9040fc4
Cherise Foster <cherise.foster@outlook.com>
2023-03-10 13:37:17 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email) {
+    const configObj = {
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        }),
+    }
+    return fetch('http://localhost:3000/users', configObj)
+     .then(resp => resp.json())
+     .then(newUser => document.body.innerHTML = newUser.id)
+     .catch(error => document.querySelector('body').innerHTML = error)
+}
